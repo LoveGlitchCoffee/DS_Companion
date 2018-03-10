a4a8c6b60c49dfdbd157a09ce8de53fafb950400
Hung <hung.h.progdev@gmail.com>
2018-03-10 17:01:41 +0000

    Updated modinfo and preview. First build on workshop

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,21 @@
-name = "Smart Companion"
-description = "Companions are smart with goal based behaviour" 
+name = "Companionship"
+description = [[Everyone needs companions!
+
+Gives you a robot companion (same model as wx78 so I can use his sprites hehe) that:
+1. Follows you around (unless you're running into dangerous areas, then uh y'know, they'd stay outta that cuz who would follow you without questions)
+2. Helps you find food when you're hungry
+3. Can be commanded to gather certain items, just click on it and pick.
+4. Doesn't go hungry or insane as lacking those modules, but dies from combat
+
+NOTE: Currently only compatible with vanilla Don't Starve 
+
+NOTE: This mod is still under development, desired features:
+1. Build the robot, in case it breaks, or just to have multiple companions?
+2. Help you with other stuff like gathering resource and combat
+
+NOTE: This mod was developed as part of an undergrad degree project, so complying by ethical practises: Companion will kill frogs for food, no additional violent content was added to vanilla Don't Starve through this mod.
+
+The idea of this mod was to give a companion kind of like in other games such as Fallout and Elder Scrolls, but improve on the aspect of not doing stuff that will harm itself (i.e. follow you right into a tentacle snare when you're doing some precision collecting)]]
 author = "ParityB1t"
 forumthread = ""
 
